f9ac9147b69e8b4d1e38f62de3797ca44ad65824
[name] <[franciscodufey@gmail.com]>
2017-04-18 10:42:11 -0300

    Upload

---
+++ animaciones.js
@@ -0,0 +1,21 @@
+
+
+$(document).ready(function () {
+
+ // Animaciones de nubes
+
+
+
+ // Generales al inicio
+
+  $('.about').hide();
+
+  $('#Sobremibtn').click(function () {
+
+    $('.about').fadeIn(1500);
+
+    });
+
+
+
+});
